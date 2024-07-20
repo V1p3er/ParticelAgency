3cf92983449a8cfb1a480f4ed1f366e9d3708c77
v1p3er <cybertechik@protonmail.com>
2024-07-21 02:25:55 +0330

    finished product

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='pershdev-home'),
+    path('projects/page-1', views.projects_pg1, name='pershdev-projects-pg1'),
+    path('projects/page-2', views.projects_pg2, name='pershdev-projects-pg2'),
+    path('about-us', views.cs50d, name='pershdev-cs50d')
+]
---
+++ home/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+    if request.method == "POST":
+        cf_name = request.POST['cf-name']
+        cf_email = request.POST['cf-email']
+        cf_number = request.POST['cf-number']
+        cf_budgets = request.POST['cf-budgets']
+        cf_message = request.POST['cf-message']
+        #send contact email
+        send_mail(
+            cf_name,
+            cf_message,
+            cf_email,
+            ['blintues@yahoo.com'],
+            cf_budgets,
+            cf_number,
+        )
+        return render(request,'home/index.html', {'cf_name':cf_name})
+    else:
+        return render(request,'home/index.html')
+
+
+def projects_pg1(request):
+    return render(request, 'home/projects-pg1.html')
+def projects_pg2(request):
+    return render(request, 'home/projects-pg2.html')
+def cs50d(request):
+    return render(request, 'home/cs50d.html')
