4910672efdf53955274d42b5f77a58bcd5471369
v1p3er <cybertechik@protonmail.com>
2024-09-27 17:35:27 +0330

    deploy

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6h24(!fd*m%ar4^%mn65u=r-6@rdeb!b$rht8_ga@6ffsj*wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'testcss')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
